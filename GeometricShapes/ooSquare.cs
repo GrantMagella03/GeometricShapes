63c1aaea0fde25ec50e0e1cc17e4866fd850099d
Grant Magella <Bitman2003@gmail.com>
2023-08-21 14:46:32 -0400

    style edits

--- GeometricShapes/ooSquare.cs
+++ GeometricShapes/ooSquare.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GeometricShapes
-{
-    internal class ooSquare : ooRect
-    {
-        public ooSquare(int S) : base(S,S)
-        {
+namespace GeometricShapes {
+    internal class ooSquare : ooRect {
+        public ooSquare(int S) : base(S,S) {
 
         }
     }
